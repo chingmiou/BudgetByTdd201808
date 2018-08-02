827a60e9f8a063d8623edcb57cc9e3adeb0c28f1
JANET\JanetWang <ching.miou@gmail.com>
2018-08-02 23:38:44 +0800

    1. pass test: no budgets

---
+++ BudgetByTdd/BudgetByTdd/Accounting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BudgetByTdd
+{
+    public class Accounting
+    {
+        private IBudgetRepository _budgetRepository;
+
+        public Accounting(IBudgetRepository budgetRepository)
+        {
+            this._budgetRepository = budgetRepository;
+        }
+
+        public decimal TotalAmount(DateTime start, DateTime end)
+        {
+            return 0m;
+        }
+    }
+}
--- BudgetByTdd/BudgetByTdd/AccountingTests.cs
+++ BudgetByTdd/BudgetByTdd/AccountingTests.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
 
 namespace BudgetByTdd
 {
     [TestClass]
     public class AccountingTests
     {
+        [TestMethod]
+        public void no_budgets()
+        {
+            var budgetRepository = Substitute.For<IBudgetRepository>();
+            var accounting = new Accounting(budgetRepository);
+            var start = new DateTime(2018, 6, 1);
+            var end = new DateTime(2018, 6, 1);
+            var totalAmount = accounting.TotalAmount(start, end);
+            Assert.AreEqual(0m, totalAmount);
+        }
     }
 }
