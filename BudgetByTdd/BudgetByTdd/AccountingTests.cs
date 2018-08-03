fe9a7205ea123e9766096e8f51ba28008bed6b35
JANET\JanetWang <ching.miou@gmail.com>
2018-08-03 22:42:50 +0800

    10. pass test: period overlap budget firstday

--- BudgetByTdd/BudgetByTdd/AccountingTests.cs
+++ BudgetByTdd/BudgetByTdd/AccountingTests.cs
@@ -47,6 +47,13 @@ namespace BudgetByTdd
             AmountShouldBe(1m, "20180531", "20180601");
         }
 
+        [TestMethod]
+        public void period_overlap_budget_firstday()
+        {
+            GivenBudgets(new Budget { YearMonth = "201805", Amount = 31 });
+            AmountShouldBe(1m, "20180430", "20180501");
+        }
+
         private void GivenBudgets(params Budget[] budgets)
         {
             _budgetRepository.GetAll().Returns(budgets.ToList());
