c46c5070b6892dfcb3d740972ece24a70bc9a757
JANET\JanetWang <ching.miou@gmail.com>
2018-09-05 23:34:05 +0800

    4. extract method Days() and move into Period class and refactor testing GivenBudgets

--- BudgetByTdd/BudgetByTdd/Accounting.cs
+++ BudgetByTdd/BudgetByTdd/Accounting.cs
@@ -3,6 +3,18 @@ using System.Linq;
 
 namespace BudgetByTdd
 {
+    internal class Period
+    {
+        public Period(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+    }
+
     internal class Accounting
     {
         private IBudgetRepository _budgetRepository;
@@ -15,11 +27,18 @@ namespace BudgetByTdd
         public decimal TotalAmount(DateTime start, DateTime end)
         {
             var budgets = _budgetRepository.GetAll();
+            var period = new Period(start, end);
             if (budgets.Any())
             {
-                return (end - start).Days + 1;
+                return Days(period);
             }
             return 0;
+        }
+
+        private static int Days(Period period)
+        {
+            var days = (period.End - period.Start).Days + 1;
+            return days;
         }
     }
 }
